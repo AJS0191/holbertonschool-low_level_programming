a836f1634504e07765c3fad1a4554559d78a0efe
AJS9091 <ashtonjamess@live.com>
2021-09-27 13:13:33 -0700

    initial push of task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point for this program
+ * paramater - void
+ *
+ *Description: Is the entry point of the program and also prints a line to stdo
+ *
+ *Return: Returned value is 0.
+ */
+
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
