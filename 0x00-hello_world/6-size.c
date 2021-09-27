450d9afb0175acc32daa5abc32b4318fb7ad928d
AJS9091 <ashtonjamess@live.com>
2021-09-27 14:23:22 -0700

    initial push of task 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point for this program
+ * paramater - void
+ *
+ *Description: Is the entry point of the program and also prints sizes of types
+ *
+ *Return: Returned value is 0.
+ */
+
+
+int main(void)
+{
+	printf("Size of a char: %zu byte(s)/n", sizeof(char));
+	printf("Size of an int: %zu byte(s)/n", sizeof(int));
+	printf("Size of a long int: %zu byte(s)/n", sizeof(long int));
+	printf("Size of a long long int: %zu byte(s)/n", sizeof(long long int);
+	printf("Size of a float: %zu byte(s)/n", sizeof(float);
+	return (0);
+}
