7647d7c1abf6b2161921ff06043413aebc1cbcf4
AJS9091 <ashtonjamess@live.com>
2021-09-27 14:40:14 -0700

    fixing new lines

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,10 +14,10 @@ int main(void)
 	long int longint; /* must not contain spaces for printf */
 	long long int longlongint;
 
-	printf("Size of a char: %zu byte(s)/n", sizeof(char));
-	printf("Size of an int: %zu byte(s)/n", sizeof(int));
-	printf("Size of a long int: %zu byte(s)/n", sizeof(long int));
-	printf("Size of a long long int: %zu byte(s)/n", sizeof(long long int));
-	printf("Size of a float: %zu byte(s)/n", sizeof(float));
+	printf("Size of a char: %zu byte(s)\n", sizeof(char));
+	printf("Size of an int: %zu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %zu byte(s)\n", sizeof(float));
 	return (0);
 }
