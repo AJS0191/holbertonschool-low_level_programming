f9dc45cce85ac26a168bff3ac9fc5f890aab173c
AJS9091 <ashtonjamess@live.com>
2021-09-28 14:46:27 -0700

    initial push of task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - entry point to program
+ * VOID
+ *
+ * Description: function prints numbers under 10
+ *
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int n=0;
+
+	for (n = 0; n < 10; n++) {
+		putchar(n);
+	}
+	return (0);
+}
