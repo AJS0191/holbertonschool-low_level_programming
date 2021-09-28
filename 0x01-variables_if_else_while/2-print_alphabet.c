94a650058811634e9869249833642652f5ce6f3f
AJS9091 <ashtonjamess@live.com>
2021-09-28 12:36:18 -0700

    Initial add task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - entry point to program
+ * VOID
+ *
+ * Description: Gives a random number than evalutes the last digit and prints
+ * results
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char alpha;
+
+	for (alpha = 'a' ; alpha <= 'z' ; alpha++)
+	{
+		putchar(alpha);
+	}
+	return (0);
+}
