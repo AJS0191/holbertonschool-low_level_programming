3fa505327833b8f0922e9ae996fbd25207c8b791
AJS9091 <ashtonjamess@live.com>
2021-09-28 17:09:54 -0700

    condition test

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - entry point to program
+ * VOID
+ *
+ * Description: Print the single digits of base 16
+ *
+ *
+ * Return: 0
+ */
+
+int main(void)
+
+{
+	unsigned int a = '0';
+	char x = 'a';
+
+	while (a <= '9')
+	{
+		putchar(a);
+		a++;
+	}
+	while (x <= 'f')
+	{
+		putchar(x);
+		x++;
+	}
+	putchar('\n');
+	return (0);
+}
