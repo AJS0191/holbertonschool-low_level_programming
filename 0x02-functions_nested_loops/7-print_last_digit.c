b8653cc50a30eaba5119004c5f1480dabf3ca192
AJS9091 <ashtonjamess@live.com>
2021-09-29 21:46:39 -0700

    initial push of task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_last_digit - prints last digit of a number
+ * @int: interger to be checked
+ *
+ * Description: takes an interger and returns the last digit
+ *  *
+ * Return: returns the last digit of input
+ */
+
+int print_last_digit(int);
+
+int print_last_digit(int n)
+{
+	n = n % 10;
+	return(n);
+}
