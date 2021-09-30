d15b404b9f42810128875779e8a987eda88cc3f8
AJS9091 <ashtonjamess@live.com>
2021-09-30 09:45:49 -0700

    initial push of task 8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+* jack_bauer - every minute of a 24hour day
+* @:
+*
+* Description: prints every minute in 24 hours in XX:XX format
+*
+* Return: VOID
+*/
+void jack_bauer(void);
+
+void jack_bauer(void)
+{
+	int min = 0;
+	int hr = 0;
+
+	while (hr < 24)
+	{
+		while (min < 60)
+		{
+		int hfd = hr / 10;
+		int hsd = hr % 10;
+		int mfd = min / 10;
+		int msd = min % 10;
+
+		_putchar(hfd + '0');
+		_putchar(hsd + '0');
+		_putchar(':');
+		_putchar(mfd + '0');
+		_putchar(msd + '0');
+		min++;
+		hr++;
+		}
+	}
+}
