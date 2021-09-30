0453896e0cc765327ee4abf69c18b154c4ad8eca
AJS9091 <ashtonjamess@live.com>
2021-09-29 21:26:40 -0700

    initial push of task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _abs- prints absolute value
+ * @int: interger to be checked
+ *
+ * Description: takes an interger and returns the absolute value
+ *
+ *
+ * Return: 0 for 0, -1 for negatives, 1 for positives
+ */
+int _abs(int);
+
+int _abs(int a)
+{
+	if (a < 0)
+	{
+		a = -a;
+	}
+	else
+	{
+		_putchar(a);
+	}
+	return(0);
+}
