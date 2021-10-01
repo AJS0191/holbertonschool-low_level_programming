329c7ded354a4934a2ffedafa61f7f43fdde22a1
AJS9091 <ashtonjamess@live.com>
2021-09-30 17:58:11 -0700

    first push of task 11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,156 @@
+#include "main.h"
+/**
+ * print_to_98 - prints given int then a sequence +1 .. +98
+ * @n: starting point
+ *
+ * Description: a loop where n is increased by one and printed
+ * Return: VOID
+ */
+void print_to_98(int n);
+
+void print_to_98(int n)
+{
+	int fn;
+	int mcn;
+	int mn;
+	int ln;
+	int a;
+
+	if (n == 98)
+	{
+		_putchar(9 + '0');
+		_putchar(8 + '0');
+	}
+	else
+	{
+		if (n > 99)
+		{
+			fn = n / 100;
+			mcn = n / 10;
+			mn = mcn % 10;
+			ln = n % 10;
+
+			_putchar(fn + '0');
+			_putchar(mn + '0');
+			_putchar(ln + '0');
+		}
+		else if (n > 9)
+		{
+			fn = n / 10;
+			ln = n % 10;
+
+			_putchar(fn + '0');
+			_putchar(ln + '0');
+		}
+		else if (n == 0)
+		{
+			_putchar('0');
+		}
+		else if (n < 0)
+		{
+			a = n * -1;
+			print_sign(n);
+			if (n > -10)
+			{
+				_putchar(a + '0');
+			}
+			else if (n < -99)
+			{
+				fn = a / 100;
+				mcn = a / 10;
+				mn = mcn % 10;
+				ln = a % 10;
+
+				_putchar(fn + '0');
+				_putchar(mn + '0');
+				_putchar(ln + '0');
+			}
+			else
+			{
+				fn = a / 10;
+				ln = a % 10;
+
+				_putchar(fn + '0');
+				_putchar(ln + '0');
+			}
+		}
+		else
+		{
+			_putchar(n);
+		}
+	}
+	if (n < 98)
+	{
+		while (n < 98)
+		{
+			if (n > 9)
+			{
+				fn = n / 10;
+				ln = n % 10;
+
+				_putchar(fn + '0');
+				_putchar(ln + '0');
+			}
+			else if (n == 0)
+			{
+				_putchar('0');
+			}
+			else if (n < 0)
+			{
+				a = n * -1;
+				print_sign(n);
+				if (n > -10)
+				{
+					_putchar(a + '0');
+				}
+				else if (n < -99)
+				{
+					fn = a / 100;
+					mcn = a / 10;
+					mn = mcn % 10;
+					ln = a % 10;
+
+					_putchar(fn + '0');
+					_putchar(mn + '0');
+					_putchar(ln + '0');
+				}
+				else
+				{
+					fn = a / 10;
+					ln = a % 10;
+
+					_putchar(fn + '0');
+					_putchar(ln + '0');
+				}
+			}
+			else
+			{
+				_putchar(n);
+			}
+			n++;
+		}
+	}
+	if (n > 98)
+	{
+		if (n > 99)
+		{
+			fn = n / 100;
+			mcn = n / 10;
+			mn = mcn % 10;
+			ln = n % 10;
+
+			_putchar(fn + '0');
+			_putchar(mn + '0');
+			_putchar(ln + '0');
+		}
+		else
+		{
+			fn = n / 10;
+			ln = n % 10;
+
+			_putchar(fn + '0');
+			_putchar(ln + '0');
+		}
+		n--;
+	}
+}
