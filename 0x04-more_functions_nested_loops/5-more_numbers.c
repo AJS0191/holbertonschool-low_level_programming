4c0d7434fb6f2de47ed8edac8e9e966f5650d15b
AJS9091 <ashtonjamess@live.com>
2021-10-04 14:46:50 -0700

    initial push for task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+
+/**
+ * more_numbers - prints 1 - 14 ten times.
+ *
+ *
+ *
+ * Return: VOID
+ **/
+
+void more_numbers(void);
+
+void more_numbers(void)
+
+{
+	int i;
+	int j;
+
+	while (j < 10)
+	{
+		i = 0;
+		while (i <= 14)
+		{
+			_putchar(i + '0');
+			i++;
+		}
+	_putchar('\n');
+	j++;
+	}
+}
