bd30045c426e031167fba17a90566b2bec2d6c91
AJS9091 <ashtonjamess@live.com>
2021-10-04 14:55:41 -0700

    fixing two digit number output

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -16,16 +16,30 @@ void more_numbers(void)
 {
 	int i;
 	int j;
+	int k;
+	int l;
 
 	while (j < 10)
 	{
 		i = 0;
 		while (i <= 14)
 		{
+			if (i < 10)
+			{
 			_putchar(i + '0');
 			i++;
+			}
+			else
+			{
+				k = i \ 10;
+				l = i % 10;
+
+				_putchar(k + '0');
+				_putchar(l + '0');
+				i++;
+			}
 		}
-	_putchar('\n');
-	j++;
+		_putchar('\n');
+		j++;
 	}
 }
