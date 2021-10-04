7c0024e4f03609b41c281e8637424a8430ea9d92
AJS9091 <ashtonjamess@live.com>
2021-10-04 13:06:32 -0700

    initial push of 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - Takes an integer and returns whether it is 0 - 9.
+ *
+ * @a: The first integer to be multiplied
+ * @b: The second integer to be multiplied
+ *
+ * Return: 1 if upper. 0 if otherwise.
+ **/
+
+int mul(int a, int b);
+
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+	return (c);
+}
