247dffe9be6aa998122365e17fe757132105169c
AJS9091 <ashtonjamess@live.com>
2021-10-04 15:08:53 -0700

    function to print 2 digit numbers

---
+++ 0x04-more_functions_nested_loops/doubled.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * doubled - takes double digit int and prints both digits
+ *
+ * @c: int to be printed
+ *
+ *void
+ **/
+
+void doubled(int c);
+
+void doubled(int c)
+{
+	a = c / 10;
+	b = c % 10;
+
+	_putchar(k + '0');
+	_putchar(l + '0');
+}
