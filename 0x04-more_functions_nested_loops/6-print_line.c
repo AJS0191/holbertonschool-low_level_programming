1ff8460ea2430e3cdc7e7b1b646dce0cfe9a00f6
AJS9091 <ashtonjamess@live.com>
2021-10-04 15:36:29 -0700

    push of task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+
+/**
+ * print_line - prints _ n times
+ * @n: number of _
+ *
+ *
+ * Return: VOID
+ **/
+
+void print_line(int n);
+
+void print_line(int n)
+
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar('_');
+	}
+}
