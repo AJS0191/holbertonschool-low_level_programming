8ff447e3d32972a51b9d854c221f16493ad122b3
AJS9091 <ashtonjamess@live.com>
2021-10-03 20:35:11 -0700

    Initial push of task 0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+
+#include "main.h"
+/**
+ * main - will test funtion that test if integer is positive or negative
+ * Return: 0
+ */
+
+int main main(void)
+{
+	int i;
+
+	i = 840;
+	positive_or_negative(i);
+
+	return (0);
+}
