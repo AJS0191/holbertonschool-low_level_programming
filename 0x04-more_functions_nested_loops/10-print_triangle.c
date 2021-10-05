143d940c962071de1b3ef4ecedc14e4d052fcda7
AJS9091 <ashtonjamess@live.com>
2021-10-05 16:09:28 -0700

    initial push for task 10

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,59 @@
+#include "main.h"
+/**
+ * print_triangle - prints # in a triangle
+ * @size: how bigs the triangle
+ *
+ *
+ * Return: VOID
+ **/
+
+void print_triangle(int size);
+
+void print_triangle(int size)
+
+{
+	int spaces;
+	int lines;
+	int hashes;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		lines = 0;
+		spaces = lines;
+		hashes = 0;
+		while (lines < size)
+		{
+			while (spaces < size)
+			{
+				_putchar(' ');
+				spaces++;
+			}
+			if (lines == 0)
+			{
+				_putchar('#');
+				hashes++;
+				_putchar('\n');
+				lines++;
+				hashes = 0;
+			}
+			else
+			{
+
+				while (hashes <= lines)
+				{
+					_putchar('#');
+					hashes++;
+				}
+				_putchar('\n');
+				spaces = 0;
+				hashes = 0;
+				lines++;
+			}
+			spaces = lines;
+		}
+	}
+}
