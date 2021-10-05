1cb7905945e8d817745402179f093b1a57048604
AJS9091 <ashtonjamess@live.com>
2021-10-04 17:07:20 -0700

    remove doubled

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,7 +11,6 @@
 void more_numbers(void);
 
 void more_numbers(void)
-
 {
 	int i;
 	int j;
@@ -21,16 +20,8 @@ void more_numbers(void)
 		i = 0;
 		while (i <= 14)
 		{
-			if (i < 10)
-			{
 			_putchar(i + '0');
 			i++;
-			}
-			else
-			{
-				doubled(i);
-				i++;
-			}
 		}
 		_putchar('\n');
 		j++;
