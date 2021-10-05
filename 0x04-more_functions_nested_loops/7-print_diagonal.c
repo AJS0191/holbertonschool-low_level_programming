f80f6649a6c14a518ba3e9813bb56c271d6d72d6
AJS9091 <ashtonjamess@live.com>
2021-10-04 17:17:39 -0700

    push task 7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+
+/**
+ * print_line - prints _ n times
+ * @n: number of _
+ *
+ *
+ * Return: VOID
+ **/
+
+void print_line(int n);
+
+void print_line(int n)
+
+{
+	int a;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		a = 0;
+
+		while (a < n)
+		{
+			_putchar('\\');
+			a++;
+		}
+		_putchar('\n');
+	}
+}
