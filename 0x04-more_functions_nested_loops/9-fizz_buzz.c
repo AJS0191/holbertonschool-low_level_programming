2eb8dab01d96caefe14b51d434d23023ee16c3e9
AJS9091 <ashtonjamess@live.com>
2021-10-05 13:37:35 -0700

    intial push of task 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+/**
+ * main - entry point of program
+ *
+ *
+ * Return: void
+ **/
+
+
+int main(void)
+{
+	int soda;
+	int x3;
+	int x5;
+
+	soda = 0;
+	while (soda < 101)
+	{
+		soda++;
+		x3 = soda % 3;
+		x5 = soda % 5;
+
+		if ((x3 == 0) && (x5 == 0))
+		{
+			printf("FizzBuzz");
+		}
+		else if (x3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (x5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%d", soda);
+		}
+		putchar(' ');
+	}
+	putchar('\n');
+}
