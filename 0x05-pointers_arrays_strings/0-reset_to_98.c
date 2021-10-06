3a1d4ce810f658d95bdcb31eb287355e25299a0d
AJS9091 <ashtonjamess@live.com>
2021-10-05 19:21:37 -0700

    initial push of task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * reset_to_98 - updates the value pointer n points to
+ *
+ *@n: pointer in which value is updated
+ *
+ **/
+
+void reset_to_98(int *n);
+
+void reset_to_98(int *n)
+{
+
+	*n = 98;
+}
