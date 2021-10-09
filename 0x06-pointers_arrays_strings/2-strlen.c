87deb385fc18198e4e2c06e2acd46dad35ba94eb
AJS9091 <ashtonjamess@live.com>
2021-10-08 21:47:43 -0700

    strlen

---
+++ 0x06-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * _strlen - takes a string and return its length
+ *
+ * @s: string to be counted
+ *
+ * Return: returns the length of s
+ **/
+int _strlen(char *s);
+
+int _strlen(char *s)
+{
+	int a = 0;
+	int counter = 0;
+
+	while (counter <= a)
+	{
+		if (s[a] != '\0')
+		{
+			a++;
+		}
+		else if (s[a] == '\0')
+		{
+			return (a);
+		}
+		counter++;
+	}
+	return (a);
+}
