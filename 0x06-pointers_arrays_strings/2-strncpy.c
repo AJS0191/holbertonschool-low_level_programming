15a47b9044345a051b330c6662f0f601afbcab29
AJS9091 <ashtonjamess@live.com>
2021-10-10 09:58:35 -0700

    account for src < n

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include "2-strlen.c"
 /**
  * _strncpy - copies n bytes of src into the beginning
  *
@@ -13,12 +14,24 @@ char *_strncpy(char *dest, char *src, int n);
 
 char *_strncpy(char *dest, char *src, int n)
 {
-
-	int a = 0;
+	int a = _strlen(dest);
 	int b = 0;
+	int c = _strlen(dest);
 
-
-
+	if (c < n)
+	{
+		while (b < c)
+		{
+			dest[a] = src[b];
+			a++;
+			b++;
+		}
+		while (b < c)
+		{
+			dest[a] = '\0';
+			b++;
+		}
+	}
 	while (b < n)
 	{
 		dest[a] = src[b];
