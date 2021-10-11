3d8a781f74dff226d4d3bc8822f240f7b7dc02e6
AJS9091 <ashtonjamess@live.com>
2021-10-11 13:25:11 -0700

    push task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include "2-strlen.c"
+/**
+ * _strchr - searches for char matching c
+ *
+ * @s: string being searched
+ * @c: character being searched for
+ *
+ * Return: string point to firs occurence of c if found, null if not
+ **/
+
+char *_strchr(char *s, char c);
+
+char *_strchr(char *s, char c)
+{
+	int a = 0;
+	char **find;
+	int b = _strlen(s);
+	char *d;
+
+	while (a < b)
+	{
+		if (s[a] == c)
+		{
+			d = &s[a];
+			find = &d;
+			return (*find);
+		}
+		a++;
+	}
+	*d = '\0';
+	find = &d;
+	return (*find);
+}
