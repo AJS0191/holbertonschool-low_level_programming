b0bcf820ed46839d541edbc8d10a6a2e9d50770f
AJS9091 <ashtonjamess@live.com>
2021-10-11 12:25:06 -0700

    push task 1

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _memcpy - copies n bytes of src into dest
+ *
+ * @dest: string be copied into
+ * @src: string being copied from
+ * @n: amount of bytes to be overwritten
+ *
+ * Returns: a pointer to dest
+ **/
+
+char *_memcpy(char *dest, char *src, unsigned int n);
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	char **copy;
+	unsigned int a = 0;
+
+	copy = &dest;
+	while (a < n)
+	{
+		dest[a] = src[a];
+		a++;
+	}
+	return (*copy);
+}
