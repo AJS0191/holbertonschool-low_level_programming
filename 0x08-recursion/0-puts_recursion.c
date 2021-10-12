5372b615a2d1d5b283fb9956cc059b7b797c9ebd
AJS9091 <ashtonjamess@live.com>
2021-10-12 10:38:26 -0700

    push task 0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _puts_recursion - prints a string
+ *
+ * @s: string being printed
+ *
+ * Return: VOID
+ **/
+void _puts_recursion(char *s);
+
+void _puts_recursion(char *s)
+{
+	if (s[0] != '\0')
+	{
+		_putchar(s[0]);
+		s++;
+		_puts_recursion(s);
+	}
+	else
+	{
+		_putchar('\0');
+	}
+}
