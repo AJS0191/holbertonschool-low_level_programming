5a5623382793ae1bdf47dac44e31b82cc2d82bd1
AJS9091 <ashtonjamess@live.com>
2021-10-11 18:55:47 -0700

    push of task 5

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,49 @@
+#include "main.h"
+#include "2-strlen.c"
+/**
+ * _strstr - finds exact match in 2 string
+ *
+ * @haystack: string being examined
+ * @needle: string being compared against
+ *
+ * Return: returns the location of full match
+ **/
+
+char *_strstr(char *haystack, char *needle);
+
+char *_strstr(char *haystack, char *needle)
+{
+	int a = 0;
+	int b = 0;
+	int c = _strlen(haystack);
+	int d = _strlen(needle);
+	char *e;
+	char **find;
+
+	while (a < c)
+	{
+		if (haystack[a] == needle[b])
+		{
+			e = &haystack[a];
+			find = &e;
+			a++;
+			b++;
+			while (b < d)
+			{
+				if (haystack[a] == needle[a])
+				{
+					a++;
+					b++;
+				}
+				else
+					break;
+				return (*find);
+			}
+		}
+		a++;
+		b++;
+	}
+	e = '\0';
+	find = &e;
+	return (*find);
+}
