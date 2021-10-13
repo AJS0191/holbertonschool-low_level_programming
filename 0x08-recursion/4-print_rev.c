5727d53d05a6e9a09927c046f7624f5ba4b978e1
AJS9091 <ashtonjamess@live.com>
2021-10-12 20:12:22 -0700

    proto

---
+++ 0x08-recursion/4-print_rev.c
@@ -0,0 +1,46 @@
+#include "main.h"
+/**
+ * print_rev - takes a string and prints in reverse
+ *
+ * @s: string to be printed
+ *
+ * RETURN: VOID
+ **/
+
+void print_rev(char *s);
+
+void print_rev(char *s)
+{
+	int a = 0;
+	int counter = 0;
+	int b;
+
+	while (counter <= a)
+	{
+		if (s[a] != '\0')
+		{
+			a++;
+		}
+		else if (s[a] == '\0')
+		{
+			a--;
+			b = a;
+			counter = a + 2;
+		}
+	}
+	counter = b;
+
+	while (counter >= b)
+	{
+		if (b >= 0)
+		{
+			_putchar(s[b]);
+			b--;
+		}
+		else
+		{
+			_putchar('\n');
+			counter = b - 1;
+				}
+	}
+}
