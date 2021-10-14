5802281248259a18968e88d33634ee94fe431504
AJS9091 <ashtonjamess@live.com>
2021-10-14 12:05:25 -0700

    push task 3

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * factorial - takes an integer and returns its factorial
+ *
+ * @n: integer being worked on
+ *
+ * Return: an int of !n or -1 on error
+ **/
+
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	if (n == 0)
+		return (1);
+	return (factorial(n - 1));
+}
