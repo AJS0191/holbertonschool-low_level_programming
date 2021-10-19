ebc2a12267733b494061730cac2fbac707b37159
AJS9091 <ashtonjamess@live.com>
2021-10-19 08:29:49 -0700

    push task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - main will print it's own name
+ *
+ * @argc: number of command line arguments
+ * @argv: string holding commands
+ *
+ * Return: 0
+ **/
+
+int main(int argc, char *argv[])
+{
+	argc = argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
