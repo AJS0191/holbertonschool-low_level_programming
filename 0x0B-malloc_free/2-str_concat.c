58cecc9ab76e718e380a22717404231f8e021717
AJS9091 <ashtonjamess@live.com>
2021-10-21 09:00:19 -0700

    push task 2

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * str_concat - puts two string together
+ *
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: returns a the two string together
+ **/
+
+char *str_concat(char *s1, char *s2)
+{
+	int i;
+	int c;
+	char *new = malloc(sizeof(s1) + sizeof(s2));
+
+	if (new)
+	{
+		for (i = 0; s1[i] != '\0'; i++)
+		{
+			new[i] = s1[i];
+		}
+		c = i;
+		for (i = 0; s2[i] != '\0'; i++)
+		{
+			new[c] = s2[i];
+			c++;
+		}
+		new[c] = s2[i];
+		return (new);
+	}
+	else
+		return (NULL);
+}
