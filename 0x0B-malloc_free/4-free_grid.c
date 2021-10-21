4b377e943f46df3a745bd24ed0c37fbfafc999e3
AJS9091 <ashtonjamess@live.com>
2021-10-21 15:27:23 -0700

    push task 4

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * free_grid - frees a 2d array
+ *
+ * @grid: grid being freed
+ * @height:height of grid
+ *
+ * RETURN: VOID
+ **/
+
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+		free(grid[i]);
+
+	free(grid);
+}
