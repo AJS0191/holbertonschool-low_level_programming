ff08c1b1788100708916b4d9692cce72392ba336
AJS9091 <ashtonjamess@live.com>
2021-10-21 15:29:59 -0700

    dont free whats not used

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -15,11 +15,11 @@ int **alloc_grid(int width, int height)
 	int j;
 	int **grid = (int **)malloc(height * sizeof(int *));
 
-	if (grid)
-	{
 	for (i = 0; i < height; i++)
 		grid[i] = (int *)malloc(width * sizeof(int));
-
+	
+	if (grid)
+	{
 	if (width == 0 || height == 0)
 	{
 		for (i = 0; i <= height; i++)
