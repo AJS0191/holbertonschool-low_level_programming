b63d567b4f3851e8c575026a5b02f54d82064ce2
AJS9091 <ashtonjamess@live.com>
2021-10-22 12:58:09 -0700

    push 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - multiplies numbers two number together
+ *
+ * @argc: number of arguments
+ * @argv: numbers to be added together
+ *
+ * Return: 0 if success, 1 if failed
+ **/
+
+int main(int argc, char *argv[])
+{
+	int a, b, p;
+
+	if (argc == 0 || argc > 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	a = atoi(argv[1]);
+	b = atoi(argv[2]);
+	p = a * b;
+	return (p);
+}
