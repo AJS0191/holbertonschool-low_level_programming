f8866a7b71d12796bbb58cc5a453a7109a1ac3e0
AJS9091 <ashtonjamess@live.com>
2021-10-22 13:47:57 -0700

    push 4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+/**
+ * main - multiplies 2 numbers
+ *
+ * @argc: number of arguments
+ * @argv: the arguments
+ *
+ * Return: 0 on success
+ **/
+
+int main(int argc, char *argv[])
+{
+	int i;
+	int j;
+	int sum;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (1);
+	}
+	for (i = 0; i <= argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (isdigit(argv[i][j]))
+			{
+				continue;
+			}
+			else
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+	}
+	for (i = 0; i < argc; i++)
+	{
+		sum += atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}
