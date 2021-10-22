abef2567b1f218e15c2e2b3f9fa402337afe0f32
AJS9091 <ashtonjamess@live.com>
2021-10-21 19:48:54 -0700

    push task 2

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - prints the name of the file
+ *
+ * @INPUT:
+ *
+ * Return: 0
+ **/
+int main(void)
+{
+
+	printf("__FILE__\n");
+	return (0);
+}
