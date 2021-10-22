500ee019ad52c6698634eb80a352f56775dad693
AJS9091 <ashtonjamess@live.com>
2021-10-22 08:02:15 -0700

    fixing malloc timing

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -13,13 +13,13 @@ int **alloc_grid(int width, int height)
 {
 	int i;
 	int j;
-	int **grid = (int **)malloc(height * sizeof(int *));
+	int **grid;
 
 	if (width == 0 || height == 0)
 	{
-		free(grid);
 		return (NULL);
 	}
+	grid = (int **)malloc(height * sizeof(int *));
 	for (i = 0; i < height; i++)
 		grid[i] = (int *)malloc(width * sizeof(int));
 	if (grid)
