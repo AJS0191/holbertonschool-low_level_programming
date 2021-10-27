d5f40f20675cc8327e056970496e6c86d1ed7163
AJS9091 <ashtonjamess@live.com>
2021-10-27 15:04:42 -0700

    push task 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_name -takes instruction on how to print string name
+ *
+ * @name: the name to be printed
+ * @f:how it is printed
+ *
+ * Return:
+ **/
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
