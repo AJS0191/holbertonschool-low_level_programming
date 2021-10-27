4460a6b4ec7ceb89c41d30ad3a99dadc8d7e63e2
AJS9091 <ashtonjamess@live.com>
2021-10-27 07:39:56 -0700

    push task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - holds info on dogs
+ * @name: * to name
+ * @age: float age
+ * @owner: * to owner name
+ *
+ * Description: holds three sets of info for each dog created
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+
+#endif /* DOG_H */
