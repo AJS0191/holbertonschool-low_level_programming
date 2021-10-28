7ac9c1ddaac7234ae95291f25bc111c44942f1f3
AJS9091 <ashtonjamess@live.com>
2021-10-28 14:58:44 -0700

    if

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -17,7 +17,7 @@ void print_dog(struct dog *d)
 			printf("Name: (nil)/n");
 		else
 			printf("Name: %s/n", d->name);
-		(d->age < 0)
+		if (d->age < 0)
 			printf("Age: (nil)");
 		else
 			printf("Age: %f/n", d->age);
