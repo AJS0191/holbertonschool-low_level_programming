8ac61e0166472213fe0bfc67a4fdc9586777a441
AJS9091 <ashtonjamess@live.com>
2021-10-28 16:57:31 -0700

    push task 5

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stddef.h>
+#include <stdlib.h>
+/**
+ * free_dog - free dog struct
+ *
+ * @d: struct being freed
+ *
+ * Return:
+ **/
+
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
