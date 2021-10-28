4f8ebdca1db19ea6fafd7d071a7cc02c8b1efbd2
AJS9091 <ashtonjamess@live.com>
2021-10-27 19:00:54 -0700

    file holding op functions

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,61 @@
+#include "main.h"
+/**
+ * op_add - performs operation
+ *
+ * @a:first num
+ * @b:second num
+ *
+ * Return: result of calculation
+ **/
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - performs operation
+ *
+ * @a:first num
+ * @b:second num
+ *
+ * Return: result of calculation
+ **/
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - performs operation
+ *
+ * @a:first num
+ * @b:second num
+ *
+ * Return: result of calculation
+ **/
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - performs operation
+ *
+ * @a:first num
+ * @b:second num
+ *
+ * Return: result of calculation
+ **/
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - performs operation
+ *
+ * @a:first num
+ * @b:second num
+ *
+ * Return: result of calculation
+ **/
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
