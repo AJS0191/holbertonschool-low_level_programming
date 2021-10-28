8bade4504d0a4e364a6a3a7f9bf843ecfa7e5db5
AJS9091 <ashtonjamess@live.com>
2021-10-27 20:13:45 -0700

    get op function for task 3

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,30 @@
+#include "3-calc.h"
+#include "3-op_functions.c"
+/**
+ * get_op_func - selects funtion based on operator
+ *
+ * @s: the argument passed
+ *
+ * Return: the correct int returning function
+ **/
+
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i;
+
+	i = 0;
+	while (i < 6)
+	{
+		if (s == ops[i].op)
+			return (ops[i].f);
+	}
+	return (NULL);
+}
