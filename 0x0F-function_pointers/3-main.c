cad73e0f620968bfd870ac9cacdc568f83c7a9dc
AJS9091 <ashtonjamess@live.com>
2021-10-27 21:54:27 -0700

    main:

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * main - takes 3 arguments and calulates
+ *
+ * @argc:
+ * @argv:
+ *
+ * Return: int
+ **/
+
+int main(int argc, char *argv[])
+{
+	int val;
+
+	if (argv[2] == NULL || argc != 3)
+	   printf("Error\n");
+
+	if (argv[2] == "/" && atoi(argv[3]) == 0)
+		printf("Error\n");
+
+	if (argv[2] == "%" && atoi(argv[3]) == 0)
+	       printf("Error\n");
+
+	val = get_op_func(argv[2])(atoi(argv[1]), atoi(argv[3]));
+	printf("%d\n", val);
+	return(0);
+}
