1295db0fee376d7f229f70443aac496a7e16b1bf
AJS9091 <ashtonjamess@live.com>
2021-10-29 19:59:40 -0700

    bones

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * sum_them_all - add all ints and returns the sum
+ *
+ * @n: number of arguments to be passed
+ *
+ * Return: the sum off number or 0 if n==0
+ **/
+
+int sum_them_all(const unsigned int n, ...);
