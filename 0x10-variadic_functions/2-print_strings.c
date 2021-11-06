9d7dc32057f75227c587a6ce051f0363000b1d49
AJS9091 <ashtonjamess@live.com>
2021-11-05 20:32:58 -0700

    account for n == 0

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdarg.h>
 #include "variadic_functions.h"
+#include <stdlib.h>
 /**
  * print_strings - takes a list of strings and prints them with a seperator
  *
@@ -16,6 +17,8 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	char *x;
 	unsigned int counter;
 
+	if (n == 0)
+		exit(0);
 	if (separator == NULL)
 	{
 		va_start(ap, n);
