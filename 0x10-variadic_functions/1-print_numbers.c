62e97ef720eb87acb9fd08e4bb88ea31468301eb
AJS9091 <ashtonjamess@live.com>
2021-11-05 19:17:57 -0700

    push task 1

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,44 @@
+#include "main.h"
+/**
+ * print_numbers - takes a list of ints and prints them to stdout
+ *
+ * @separator: string that acts as sperator for ints
+ * @n: the number of ints being printed
+ *
+ * Return: VOID
+ **/
+#include <stdio.h>
+#include <stdarg.h>
+#include "main.h"
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list ap;
+	int x, counter;
+
+	if (separator == NULL)
+	{
+		va_start(ap, n);
+		for (counter = 0; counter < n - 1; counter++)
+		{
+			x = va_arg(ap, int);
+			printf("%d", x);
+		}
+		x = va_arg(ap, int);
+		printf("%d", x);
+		va_end(ap);
+	}
+	else
+	{
+		va_start(ap, n);
+		for (counter = 0; counter < n - 1; counter++)
+		{
+			x = va_arg(ap, int);
+			printf("%d", x);
+			printf("%s", separator);
+		}
+		x = va_arg(ap, int);
+		printf("%d", x);
+		va_end(ap);
+	}
+}
