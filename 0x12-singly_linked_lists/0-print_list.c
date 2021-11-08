618c5d0cee91f2b54d4426ac64b96f1cc204fd57
AJS9091 <ashtonjamess@live.com>
2021-11-08 13:46:47 -0800

    push task 0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stddef.h>
+#include <string.h>
+/**
+ * print_list - prints elements of a list_t linked list
+ *
+ * @h: linked list to be printed
+ *
+ *
+ * Return: returns the number of nodes in the list which is of type size_t (unsigned int)
+ **/
+
+
+size_t print_list(const list_t *h)
+{
+	/* allowed to use printf */
+	const list_t *temp = h;
+	size_t nodes = 0;
+	/*prints the first nodes string
+	printf("%s", h->str);*/
+	while(temp != NULL)
+	{
+		if(temp->str == NULL)
+		{
+			printf("[0] %s\n", "(nil)");
+		}
+		else
+		{
+			printf("[%u] %s\n", temp->len, temp->str);
+		}
+		temp = temp->next;
+		nodes++;
+	}
+	return (nodes);
+}
