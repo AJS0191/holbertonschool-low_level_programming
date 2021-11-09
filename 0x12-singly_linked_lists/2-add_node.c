a44346e8a98c64c3e5353e0a90f3412c0601848c
AJS9091 <ashtonjamess@live.com>
2021-11-08 20:42:32 -0800

    add task 2

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * add_node - adds a node to a linked list
+ *
+ * @head: linked list being added too
+ * @str: string being inserted in node
+ *
+ * Return: returns the address of the updated list
+ **/
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *newn;
+
+	newn = malloc(sizeof(list_t));
+	if (!newn)
+	{
+		return (NULL);
+	}
+	newn->next = *head;
+	newn->str = (char *)str;
+	newn->len = strlen(str);
+	*head = newn;
+
+	return (*head);
+}
