4fe6f93b080b9c898cf6872138b058aae7010438
AJS9091 <ashtonjamess@live.com>
2021-11-11 11:00:20 -0800

    pushing task 0

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * print_listint - prints elements of a listint_t linked list
+ *
+ * @h: linked list to be printed
+ *
+ *
+ * Return: returns the number of nodes in the list which is of type ssize_t
+ **/
+
+size_t print_listint(const listint_t *h)
+{
+	listint_t *printer;
+	size_t node_count = 0;
+
+	printer = malloc(sizeof(listint_t));
+	if (!printer)
+		return (0);
+
+	*printer = *h;
+	while (printer->next != NULL)
+	{
+		printf("%d\n", printer->n);
+		printer = printer->next;
+		node_count++;
+	}
+	/*Have to account for last node*/
+	printf("%d\n", printer->n);
+	node_count++;
+	return (node_count);
+}
