8822cd050ff6677e7d38543fbfa111bcb5ae27d9
AJS9091 <ashtonjamess@live.com>
2021-11-11 12:26:48 -0800

    add task 3

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,45 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stddef.h>
+#include <string.h>
+/**
+ * add_nodeint_end - adds new node at end of linked list
+ *
+ * @head: pointer to head of list
+ * @n: integer being stored in new node
+ *
+ * Return: returns the number of nodes in the list which is of type size_t
+ **/
+
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *newn, *indexer;
+	int input = n;
+
+	newn = malloc(sizeof(listint_t));
+	if (!newn)
+	{
+		free(newn);
+		exit(0);
+	}
+	indexer = malloc(sizeof(listint_t));
+	if (!indexer)
+	{
+		free(indexer);
+		exit(0);
+	}
+	newn->next = NULL;
+	newn->n = input;
+	if (*head == NULL)
+		*head =newn;
+	else
+	{
+		indexer = *head;
+		while (indexer->next != NULL)
+		{
+			indexer = indexer->next;
+		}
+		indexer->next = newn;
+	}
+	return (*head);
+}
