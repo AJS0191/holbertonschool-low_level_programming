794df23b42f399e8d3ed48ea923f16a9a44e9ba3
AJS9091 <ashtonjamess@live.com>
2021-11-12 21:50:51 -0800

    task 3

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include "main.h"
+#include <string.h>
+
+/**
+ *set_bit - sets bit to 1
+ * @index: bit being worked
+ * @n: number being passed
+ * Return: new number or err
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	int x = (1 << 6) | x;
+
+	if (sizeof(n) * 8 < index)
+		return (-1);
+	return (x);
+}
