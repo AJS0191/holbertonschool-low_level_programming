ad29abd1bfbfca8a52738d66cdd45237c0d9c8d1
AJS9091 <ashtonjamess@live.com>
2021-11-12 18:43:08 -0800

    add task 0

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,49 @@
+#include <stdlib.h>
+#include "main.h"
+#include <string.h>
+
+/**
+ * power - returns x to the power of y
+ * @x: number being worked
+ * @y: power its being taken to
+ * Return: x to the power of y via recursion
+ */
+int power(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+		return (1);
+	if (y > 0)
+		return (x * power(x, y - 1));
+
+	return (x);
+}
+
+/**
+ * binary_to_uint - takes a string of binary return unsigned int
+ * @b: string of 1's and 0's
+ *
+ * Return: converts a binary number given as a string to an unsigned int
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+
+	char *swap = strdup(b);
+	unsigned int sum = 0;
+	unsigned int len = strlen(b);
+	unsigned int counter = 0;
+	char holder;
+
+	for (len = strlen(b); len > 0; len--)
+	{
+		holder = swap[counter];
+		if (holder - 48 == 1)
+		{
+			sum += power(2, len - 1);
+		}
+		counter++;
+	}
+	return (sum);
+}
