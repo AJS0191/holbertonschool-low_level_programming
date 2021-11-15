5aef1e95784b97143cd06f7a4461c38269b9633e
AJS9091 <ashtonjamess@live.com>
2021-11-15 10:56:46 -0800

    betty issues; lintint_len function; return o not null

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -3,12 +3,46 @@
 #include <stddef.h>
 #include <string.h>
 /**
- * get_nodeint_at_index - selects a specific linked list
+ * listint_len - prints elements of a list_t linked list
+ *
+ * @h:  constant linked list to be printed
+ *
+ *
+ * Return: returns the number of nodes in the list
+ **/
+
+size_t listint_len(const listint_t *h)
+{
+	listint_t *printer;
+	size_t node_count = 0;
+
+	printer = malloc(sizeof(listint_t));
+	if (!printer)
+	{
+		free(printer);
+		return (0);
+	}
+	*printer = *h;
+	while (printer->next != NULL)
+	{
+		printer = printer->next;
+		node_count++;
+	}
+	/* Last node counted here*/
+	node_count++;
+	return (node_count);
+}
+#include "lists.h"
+#include <stdio.h>
+#include <stddef.h>
+#include <string.h>
+/**
+ * sum_listint - adds all ->n ints together and returns it
  *
  * @head: linked list
- * @index: specifies node
  *
- * Return: pointer to specified node
+ *
+ * Return: int sum = all nodes->n add together
  **/
 
 int sum_listint(listint_t *head)
@@ -17,7 +51,7 @@ int sum_listint(listint_t *head)
 	int sum = 0;
 
 	if (!holder)
-		return (NULL);
+		return (0);
 
 	holder = head;
 	while(holder != NULL)
