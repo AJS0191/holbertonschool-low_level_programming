0d3965ba0f13e5b08c7fb1b4267c7288de2d5b53
AJS9091 <ashtonjamess@live.com>
2021-11-17 15:58:27 -0800

    add task 0

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,50 @@
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include "main.h"
+/**
+ * read_textfile - prints certain amount of chars from filname (cat?)
+ * @filename: name of file being opened and worked with
+ * @letters: amount of chars to be written
+ *
+ * Return: the number of letters printed
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t i = 0;
+	int fd;
+	char *buf;
+
+	/* check if filename is null*/
+	if (!filename)
+		return (0);
+
+	buf = malloc(letters + 1);
+	if (!buf)
+		return (0);
+
+	/* set fd to given filename , set to readonly*/
+	fd = open(filename, O_RDONLY);
+	/* edge case if open fails; open returns -1 on fail */
+	if (fd == -1)
+		return (0);
+
+	/* sets "letters" cha of buffer to first "letters" cha of filename*/
+	read(fd, buf, letters);
+
+	if (!buf)
+		return (0);
+	/*done with fd so close it*/
+	close(fd);
+
+	/*while loop to print and get #of chars printed*/
+
+	while (buf[i] != '\0')
+	{
+		_putchar(buf[i]);
+			 i++;
+	}
+	return (i);
+}
