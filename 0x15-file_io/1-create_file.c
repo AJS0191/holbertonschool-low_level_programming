3d4b89a0fdfa76285a6765990b38a5243655b6f0
AJS9091 <ashtonjamess@live.com>
2021-11-18 07:44:47 -0800

    change to truncate

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,7 +20,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 
 	/* opens or creates "filename" with write permission;appends if exists*/
-	fd = open(filename, O_APPEND | O_CREAT | O_WRONLY, 0600);
+	fd = open(filename, O_TRUNC | O_CREAT | O_WRONLY, 0600);
 
 	if (fd == -1)
 		return (-1);
