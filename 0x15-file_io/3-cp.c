54c432cfcfc6f71d6596e2447187ae8d788834dd
AJS9091 <ashtonjamess@live.com>
2021-11-18 08:56:25 -0800

    adding task 3

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,74 @@
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - copies a file
+ * @argc: number of args
+ * @argv: args
+ *
+ * Return: void
+ */
+
+int main(int argc, char **argv)
+{
+	char buf[1024];
+	int fd, err;
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	fd = open(argv[1], O_RDONLY);
+
+	if (fd == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+
+	err = read(fd, buf, 1024);
+
+	if (err == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+
+	err = close(fd);
+
+	if (err == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
+
+	fd = open(argv[1], O_APPEND | O_CREAT | O_WRONLY, 0661);
+
+	if (fd == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+
+	err = write(fd, buf, 1024);
+
+	if (err == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+
+	err = close(fd);
+
+	if (err == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
+	return (1);
+}
