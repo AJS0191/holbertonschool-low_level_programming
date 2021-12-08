9e043903d39cb63719c4ed440f801166a3035a79
AJS9091 <ashtonjamess@live.com>
2021-12-07 20:11:19 -0800

    adding task 3, passes local tests

---
+++ 0x18-doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,60 @@
+#include "lists.h"
+
+
+dlistint_t *get_last_node(dlistint_t *head)
+{
+	dlistint_t *last_node;
+
+	if (head->next == NULL)
+	{
+		return (head);
+	}
+	while (head->next != NULL)
+	{
+		last_node = head->next;
+		head = head->next;
+	}
+	return (last_node);
+}
+
+/**
+ * add_dnodeint_end - adds a node to the end of a dlistint_t list
+ *
+ * @head: address to the head of the list
+ * @n: content of new node
+ *
+ * Return: returns the address of the new element or NULL
+ **/
+
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *new_node;
+	dlistint_t *last_node;
+
+	new_node = malloc(sizeof(dlistint_t *));
+	if (new_node)
+	{
+		if (*head == NULL)
+		{
+			new_node->n = n;
+			new_node->next = NULL;
+			new_node->prev = NULL;
+			*head = new_node;
+			return (*head);
+		}
+
+
+		else
+		{
+			last_node = get_last_node(*head);
+			last_node->next = new_node;
+			new_node->n = n;
+			new_node->next = NULL;
+			new_node->prev = last_node;
+			return (new_node);
+		}
+	}
+	else
+		return (NULL);
+
+}
