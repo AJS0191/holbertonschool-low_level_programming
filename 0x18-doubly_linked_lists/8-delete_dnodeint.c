6970830d94868756678b492237e75ef9f4194ba1
AJS9091 <ashtonjamess@live.com>
2021-12-08 20:42:40 -0800

    complete rework; changed the way head was refrenced

--- 0x18-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x18-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,28 +1,6 @@
 #include "lists.h"
 /**
- * dlistint_len - counts the nodes in a dlistin_len list
- *
- * @h: head of the list being worked on
- *
- * Return: returns the amount of nodes as size_t
- **/
-
-size_t dlistint_len(const dlistint_t *h)
-{
-	size_t nodes = 0;
-
-	if (!h)
-		return (nodes);
-
-	while (h)
-	{
-		nodes++;
-		h = h->next;
-	}
-	return (nodes);
-}
-/**
- * delete_dnodeint_at_index - deletes a node at give index in dlistint_t list.
+ * delete_dnodeint_at_index - deletes specified node in dlistint_t list
  *
  * @head: start of the list
  * @index: specifies node to be deleted
@@ -33,42 +11,24 @@ size_t dlistint_len(const dlistint_t *h)
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
 	unsigned int i = 0;
-	dlistint_t *head_ptr = *head;
-	dlistint_t *b_ptr, *a_ptr, *d_ptr;
+	dlistint_t *head_h = *head;
 
-	if (index > dlistint_len(head_ptr))
-		return (-1);
-	if (*head == NULL)
-		return (-1);
-	if (head_ptr->next == NULL)
+	while (head_h)
 	{
-		*head = NULL;
-		return (1);
-	}
-	if (index != 0)
-	{
-		while (i < (index - 1))
+		if (i == index)
 		{
-			head_ptr = head_ptr->next;
-			i++;
+			if (head_h->prev)
+				head_h->prev->next = head_h->next;
+			else
+				*head = head_h->next;
+			if (head_h->next)
+				head_h->next->prev = head_h->prev;
+			free(head_h);
+			return (1);
 		}
-		b_ptr = head_ptr;
-		/*grab the node to be deleted*/
-		head_ptr = head_ptr->next;
-		d_ptr = head_ptr;
-		/*grab the node after*/
-		head_ptr = head_ptr->next;
-		a_ptr = head_ptr;
-		b_ptr->next = a_ptr;
-		a_ptr->prev = b_ptr;
-		free(d_ptr);
-		return (1);
+		head_h = head_h->next;
+		i++;
 	}
-	else
-	{
-		d_ptr = head_ptr->next;
-		head_ptr->next = d_ptr->next;
-		head_ptr->n = d_ptr->n;
-		free(d_ptr);
-		return (1);
-	}}
+	return (-1);
+
+}
