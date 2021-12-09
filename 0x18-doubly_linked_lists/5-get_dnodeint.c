1b8ebb3d4b47dddb59636c19ba4b3dc791b24c8b
AJS9091 <ashtonjamess@live.com>
2021-12-08 19:01:29 -0800

    adding helper function definition

--- 0x18-doubly_linked_lists/5-get_dnodeint.c
+++ 0x18-doubly_linked_lists/5-get_dnodeint.c
@@ -1,4 +1,28 @@
 #include "lists.h"
+
+/**
+ * dlistint_len - counts the nodes in a dlistin_len list
+ *
+ * @h: head of the list being worked on
+ *
+ * Return: returns the amount of nodes as size_t
+ **/
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t nodes = 0;
+
+	if (!h)
+		return (nodes);
+
+	while (h)
+	{
+		nodes++;
+		h = h->next;
+	}
+	return (nodes);
+}
+
 /**
  * get_dnodeint_at_index - grabs node at specified index
  *
@@ -13,6 +37,9 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 	unsigned int i = 0;
 
 	if (dlistint_len(head) < index)
+		return (NULL);
+
+	if (!head)
 		return (NULL);
 
 	while (i < index)
