f0d54a619351b651a0b4ba790e3ecff28f15a9ed
AJS9091 <ashtonjamess@live.com>
2021-12-08 19:21:57 -0800

    Updating with helper function definition

--- 0x18-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x18-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,5 +1,27 @@
 #include "lists.h"
 /**
+ * dlistint_len - counts the nodes in a dlistin_len list
+ *
+ * @h: head of the list being worked on
+ *
+ * Return: returns the amount of nodes as size_t
+ **/
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t nodes = 0;
+
+	if (!h)
+		return (nodes);
+
+	while (h)
+	{
+		nodes++;
+		h = h->next;
+	}
+	return (nodes);
+}
+/**
  * delete_dnodeint_at_index - deletes a node at give index in dlistint_t list.
  *
  * @head: start of the list
