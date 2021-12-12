b6404ec453213ac52f0e76fc93b2f6d8e1f15dfd
AJS9091 <ashtonjamess@live.com>
2021-12-12 15:27:58 -0800

    betty file

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -6,7 +6,7 @@
  * @key: data being used to create index
  * @size: the size of the table data will be stored in
  *
- * Return - returns the correct index for data to be stored
+ * Return: returns the correct index for data to be stored
  **/
 
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
