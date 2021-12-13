2dd5f3bca09523298c1f4407263ef791e48a72b5
AJS9091 <ashtonjamess@live.com>
2021-12-13 12:24:55 -0800

    reworking if statements

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -17,23 +17,23 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int index;
 	hash_node_t *temp;
 
-	if (ht && key)
+	if (!ht || !key)
 	{
+		return (NULL);
+	}
+	index = key_index((unsigned char *)key, size);
 
-		index = key_index((unsigned char *)key, size);
+	temp = table[index];
+	if (temp == NULL)
+		return (NULL);
+	if (strcmp(temp->key, key) == 0)
+		return (temp->value);
 
-		temp = table[index];
-		if (temp == NULL)
-			return (NULL);
-		if (strcmp(temp->key, key) > -1)
+	while (temp != NULL)
+	{
+		if (strcmp(temp->key, key) == 0)
 			return (temp->value);
-
-		while (temp->next != NULL)
-		{
-			if (strcmp(temp->key, key) > -1)
-				return (temp->value);
-			temp = temp->next;
-		}
+		temp = temp->next;
 	}
 	return (NULL);
 }
