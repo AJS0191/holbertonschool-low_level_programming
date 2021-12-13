7dbec2f5cf6cfaa0c528f1ece2d58e572beb2869
AJS9091 <ashtonjamess@live.com>
2021-12-13 11:11:56 -0800

    Allowing value to be empty

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -16,7 +16,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	if (key == NULL || strlen(key) < 1)
 		return (0);
-	if (!ht || !value)
+	if (!ht)
 		return (0);
 
 	new_node = malloc(sizeof(hash_node_t));
