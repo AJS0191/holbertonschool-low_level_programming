3c7f5c4eda658c90245cf2f26c619a294aff9ebb
AJS0191 <ashtonjamess@live.com>
2022-06-16 15:36:29 -0500

    EDIT: add recursive helper to header

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -4,4 +4,5 @@
 #include <stdlib.h>
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
+int rec_binary(int *array, size_t first, size_t last, int value);
 #endif
